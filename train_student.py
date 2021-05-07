79aa4bc84d824890b1ce43fbb221fcde9700e60c
EricZsy <37525079+EricZsy@users.noreply.github.com>
2021-05-07 17:53:07 +0800

    update

--- train_student.py
+++ train_student.py
@@ -32,7 +32,7 @@ parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet32',
                     help='model architecture: ' +
                          ' | '.join(model_names) +
                          ' (default: resnet32)')
-parser.add_argument('--loss_type', default="KD", type=str, help='loss type')
+parser.add_argument('--loss_type', default="BKD", type=str, help='loss type')
 parser.add_argument('--imb_type', default="exp", type=str, help='imbalance type')
 parser.add_argument('--imb_factor', default=0.01, type=float, help='imbalance factor')
 parser.add_argument('--train_rule', default='None', type=str, help='data sampling strategy for train loader')
